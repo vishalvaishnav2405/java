374dc86c1b595987d23725f13e94466b31c683eb
vishalvaishnav2405 <70995352+vishalvaishnav2405@users.noreply.github.com>
2020-09-14 12:33:34 +0530

    Add files via upload

---
+++ Argument.java
@@ -0,0 +1,8 @@
+public class Argument {
+    public static void main(String[] args) {
+         int a = Integer.parseInt(args[0]);
+         int b = Integer.parseInt(args[1]);
+         int sum = a + b;
+        System.out.println("The sum is" +sum);
+    }
+  }
