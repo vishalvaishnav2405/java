c9ab4fd63ca4fe3016228c8243e5aa1010e072b1
vishalvaishnav2405 <70995352+vishalvaishnav2405@users.noreply.github.com>
2020-11-05 14:33:49 +0530

    Delete Argument.java

--- Argument.java
+++
@@ -1,8 +0,0 @@
-public class Argument {
-    public static void main(String[] args) {
-         int a = Integer.parseInt(args[0]);
-         int b = Integer.parseInt(args[1]);
-         int sum = a + b;
-        System.out.println("The sum is" +sum);
-    }
-  }
