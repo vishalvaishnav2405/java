8fe4d8ccb090dcb4fc18d550eab217cde01c9c48
vishalvaishnav2405 <70995352+vishalvaishnav2405@users.noreply.github.com>
2020-11-23 16:09:11 +0530

    Update commandlineadd.java

--- commandlineadd.java
+++ commandlineadd.java
@@ -1,13 +1,11 @@
-public class commandlineadd {
-    public static void main(String args[]) {
-        int sum = 0;
-        for(int i=0;i<args.length;i++)
-
-        {
-                    sum=sum + Integer.parseInt(args[i]);
-            
-        }
-         System.out.println("The result is:"+sum);
-    }
+public class ReadNumberExample3  
+{  
+public static void main(String args[])  
+{  
+int a,b,sum;  
+a=Integer.parseInt(args[0]);   //first input number   
+b=Integer.parseInt(args[1]);  //second input number  
+sum=a+b; //adding two numbers  
+System.out.println("The sum of " + a + " and " + b +" is " +sum);  
+}  
 }
-    
